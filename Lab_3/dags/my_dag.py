99b79e98d517b41194e488cd966d84afee3c2f4c
nishipatel18 <nishipatel1918@gmail.com>
2026-02-14 14:13:03 -0500

    Update my_dag.py

--- Lab_3/dags/my_dag.py
+++ Lab_3/dags/my_dag.py
@@ -1,56 +1,86 @@
+from __future__ import annotations
+import pendulum
 from airflow import DAG
-from airflow.providers.standard.operators.python import PythonOperator
-from airflow.providers.standard.operators.bash import BashOperator
-from datetime import datetime, timedelta
-from src.model_development import load_data, data_preprocessing, build_model, load_model
-from src.success_email import send_success_email
+from airflow.operators.bash import BashOperator
+from airflow.operators.python_operator import PythonOperator
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+from airflow.utils.trigger_rule import TriggerRule
+
+from src.model_development import (
+    load_data,
+    data_preprocessing,
+    separate_data_outputs,
+    build_model,
+    load_model,
+)
 
 default_args = {
-    'owner': 'Nishi Patel',
-    'start_date': datetime(2024, 1, 1),
-    'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    "start_date": pendulum.datetime(2024, 1, 1, tz="UTC"),
+    "retries": 0,
 }
 
-with DAG(
-    'sample_dag',
+dag = DAG(
+    dag_id="Airflow_Lab2",
     default_args=default_args,
-    description='Wine Quality Random Forest Pipeline for VM Deployment',
-    schedule_interval=None,
+    description="Wine Quality Random Forest Classification - Lab2",
+    schedule="@daily",
     catchup=False,
-    tags=['wine_quality', 'vm', 'lab3'],
-) as dag:
+    tags=["wine_quality"],
+    owner_links={"Nishi Patel": "https://github.com/nishipatel18/MLOPS_AIRFLOW_LAB2/"},
+    max_active_runs=1,
+)
 
-    load_data_task = PythonOperator(
-        task_id='load_data_task',
-        python_callable=load_data,
-    )
+owner_task = BashOperator(
+    task_id="task_using_linked_owner",
+    bash_command="echo 1",
+    owner="Nishi Patel",
+    dag=dag,
+)
 
-    data_preprocessing_task = PythonOperator(
-        task_id='data_preprocessing_task',
-        python_callable=data_preprocessing,
-        op_args=[load_data_task.output],
-    )
+load_data_task = PythonOperator(
+    task_id="load_data_task",
+    python_callable=load_data,
+    dag=dag,
+)
 
-    build_model_task = PythonOperator(
-        task_id='build_model_task',
-        python_callable=build_model,
-        op_args=[data_preprocessing_task.output, "model.sav"],
-    )
+data_preprocessing_task = PythonOperator(
+    task_id="data_preprocessing_task",
+    python_callable=data_preprocessing,
+    op_args=[load_data_task.output],
+    dag=dag,
+)
 
-    load_model_task = PythonOperator(
-        task_id='load_model_task',
-        python_callable=load_model,
-        op_args=[data_preprocessing_task.output, "model.sav"],
-    )
+separate_data_outputs_task = PythonOperator(
+    task_id="separate_data_outputs_task",
+    python_callable=separate_data_outputs,
+    op_args=[data_preprocessing_task.output],
+    dag=dag,
+)
 
-    success_email_task = PythonOperator(
-        task_id='send_success_email',
-        python_callable=send_success_email,
-        provide_context=True,
-    )
+build_save_model_task = PythonOperator(
+    task_id="build_save_model_task",
+    python_callable=build_model,
+    op_args=[separate_data_outputs_task.output, "model.sav"],
+    dag=dag,
+)
 
-    load_data_task >> data_preprocessing_task >> build_model_task >> load_model_task >> success_email_task
+load_model_task = PythonOperator(
+    task_id="load_model_task",
+    python_callable=load_model,
+    op_args=[separate_data_outputs_task.output, "model.sav"],
+    dag=dag,
+)
 
-if __name__ == "__main__":
-    dag.test()
+trigger_dag_task = TriggerDagRunOperator(
+    task_id="my_trigger_task",
+    trigger_dag_id="Airflow_Lab2_Flask",
+    conf={"message": "Data from upstream DAG"},
+    reset_dag_run=False,
+    wait_for_completion=False,
+    trigger_rule=TriggerRule.ALL_DONE,
+    dag=dag,
+)
+
+owner_task >> load_data_task >> data_preprocessing_task >> \
+    separate_data_outputs_task >> build_save_model_task >> \
+    load_model_task >> trigger_dag_task
