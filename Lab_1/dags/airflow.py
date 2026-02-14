df732c6163e44d2652942d1c8b43a1710a35e67f
nishipatel18 <nishipatel1918@gmail.com>
2026-02-14 11:12:17 -0500

    Update airflow.py

---
+++ Lab_1/dags/airflow.py
@@ -0,0 +1,46 @@
+from airflow import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime, timedelta
+from src.lab import load_data, data_preprocessing, build_save_model, load_model_elbow
+
+default_args = {
+    'owner': 'Nishi Patel',
+    'start_date': datetime(2026, 1, 15),
+    'retries': 0,
+    'retry_delay': timedelta(minutes=5),
+}
+
+with DAG(
+    'Airflow_Lab1',
+    default_args=default_args,
+    description='Wine Quality Random Forest Classification Pipeline',
+    catchup=False,
+) as dag:
+
+    load_data_task = PythonOperator(
+        task_id='load_data_task',
+        python_callable=load_data,
+    )
+
+    data_preprocessing_task = PythonOperator(
+        task_id='data_preprocessing_task',
+        python_callable=data_preprocessing,
+        op_args=[load_data_task.output],
+    )
+
+    build_save_model_task = PythonOperator(
+        task_id='build_save_model_task',
+        python_callable=build_save_model,
+        op_args=[data_preprocessing_task.output, "model.sav"],
+    )
+
+    load_model_task = PythonOperator(
+        task_id='load_model_task',
+        python_callable=load_model_elbow,
+        op_args=["model.sav", build_save_model_task.output],
+    )
+
+    load_data_task >> data_preprocessing_task >> build_save_model_task >> load_model_task
+
+if __name__ == "__main__":
+    dag.test()
